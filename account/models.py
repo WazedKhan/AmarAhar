da419b89bbbde26de7ffc1260e645740d52443e6
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-21 18:03:02 +0600

    account model for user

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    image = models.ImageField(upload_to = 'Account')
+    experience = models.DecimalField()
+    type = models.CharField(max_length=100)
