5c7fe8edb6f666a2f1fe323778bf542f964ba184
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-21 17:15:43 +0600

    static url

--- AmarAhar/urls.py
+++ AmarAhar/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from AmarAhar.settings import DEBUG
 
 from graphene_django.views import GraphQLView
 
@@ -7,3 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
