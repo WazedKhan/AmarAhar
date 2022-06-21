3379b1de1a9036a3426146f594bfb42c20174c69
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-21 18:10:24 +0600

    field req

--- account/models.py
+++ account/models.py
@@ -5,5 +5,5 @@ class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
     image = models.ImageField(upload_to = 'Account')
-    experience = models.DecimalField()
+    experience = models.DecimalField(max_digits=1, null=True, blank=True)
     type = models.CharField(max_length=100)
