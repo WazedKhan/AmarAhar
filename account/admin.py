7e37c8616599f880d90bcae84b10fc05a21f6216
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-21 18:09:29 +0600

    added account model’s fields to the user page

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from account.models import Account
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class AccountInLine(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'accounts'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (AccountInLine,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
